5d3dd0ad896119e0d607b289bb35e46f5cca1b2c
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-08 16:29:46 +0200

    Added SetupControllers to LibRetroCore

--- src/LibRetroCore.h
+++ src/LibRetroCore.h
@@ -20,10 +20,10 @@ namespace RetroSim
         void SetEnvironment(retro_environment_t envCallback)
         {
             SetupLogging();
+            SetupControllers();
         }
 
-        private:
-
+    private:
         // We try gettig a callback from the frontend and set it as a backend in our Logger class.
         // If we fail, the Logger class falls back to stdio.
         void SetupLogging()
@@ -35,10 +35,28 @@ namespace RetroSim
 
             this->envCallback = envCallback;
 
-            if(envCallback(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logCallback))
+            if (envCallback(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logCallback))
             {
                 logger.SetLibRetroCallback(logCallback.log);
             }
+        }
+
+        // We define a single, "joypad" style controller as set it as the only controller.
+        // Todo: Add support for multiple controllers, suitable for the current build target.
+        void SetupControllers()
+        {
+            static const struct retro_controller_description controllers[] =
+            {
+                {"Nintendo DS", RETRO_DEVICE_SUBCLASS(RETRO_DEVICE_JOYPAD, 0)},
+            };
+
+            static const struct retro_controller_info ports[] =
+            {
+                {controllers, 1},
+                {NULL, 0},
+            };
+
+            envCallback(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void *)ports);
         }
     };
 }
