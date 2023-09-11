0ce0af1ce25de8db98bde3e4d7d3a8f15d3934cc
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-11 04:05:01 +0200

    Added header + link to font credits

--- data/unscii-16.h
+++ data/unscii-16.h
@@ -1,5 +1,8 @@
-#ifndef unscii_16_H
-#define unscii_16_H
+// RetroSim - Copyright 2011-2023 Zoltán Majoros. All rights reserved.
+// https://github.com/arcanelab
+
+#pragma once
+
+// http://viznut.fi/unscii/
 extern const int unscii_16[];
 extern const int unscii_16_length;
-#endif
--- data/unscii-8.h
+++ data/unscii-8.h
@@ -1,5 +1,8 @@
-#ifndef unscii_8_H
-#define unscii_8_H
+// RetroSim - Copyright 2011-2023 Zoltán Majoros. All rights reserved.
+// https://github.com/arcanelab
+
+#pragma once
+
+// http://viznut.fi/unscii/
 extern const int unscii_8[];
 extern const int unscii_8_length;
-#endif
