be7fdf38e3f7a5207b0c1a53ae6a76f37dcb384e
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-11 04:05:29 +0200

    Moved palette data into a separate file

---
+++ data/palette.h
@@ -0,0 +1,8 @@
+// RetroSim - Copyright 2011-2023 Zoltán Majoros. All rights reserved.
+// https://github.com/arcanelab
+
+#pragma once
+#include <stdlib.h>
+
+// https://lospec.com/palette-list/aap-64
+extern uint32_t palette_64[64];
