e1f6ef852f7989372b3794cfcefce1d5d96ad9ea
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-17 02:54:01 +0200

    Moved template definitions into the header file

--- src/MMU.cpp
+++ src/MMU.cpp
@@ -2,49 +2,12 @@
 // https://github.com/arcanelab
 
 #include "MMU.h"
-#include "Logger.h"
 
 using namespace RetroSim::Logger;
 
 namespace RetroSim::MMU
 {
-    // Explicit instantiation
-    template uint8_t ReadMem<uint8_t>(uint32_t address);
-    template uint16_t ReadMem<uint16_t>(uint32_t address);
-    template uint32_t ReadMem<uint32_t>(uint32_t address);
-
-    template void WriteMem<uint8_t>(uint32_t address, uint8_t value);
-    template void WriteMem<uint16_t>(uint32_t address, uint16_t value);
-    template void WriteMem<uint32_t>(uint32_t address, uint32_t value);
-
     MemorySections memory;
-
-    template <typename T>
-    inline T ReadMem(uint32_t address)
-    {
-        if (address < memorySize)
-        {
-            return *((T *)(memory.raw + address));
-        }
-        else
-        {
-            LogPrintf(RETRO_LOG_ERROR, "ReadMem: invalid address: %08X\n", address);
-            return 0;
-        }
-    }
-
-    template <typename T>
-    inline void WriteMem(uint32_t address, T value)
-    {
-        if (address < memorySize)
-        {
-            *(T *)(memory.raw + address) = value;
-        }
-        else
-        {
-            LogPrintf(RETRO_LOG_ERROR, "WriteMem: invalid address: %08X\n", address);
-        }
-    }
 
     int LoadFile(const char *filename, uint32_t address)
     {
