849e1c178f4083ec394711ad66e6610bb7667ab7
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-24 22:38:19 +0200

    Added 'APICalls' enum with function IDs

--- src/GPU.h
+++ src/GPU.h
@@ -15,24 +15,45 @@ namespace RetroSim::GPU
 
     void Initialize();
 
+    enum APICalls
+    {
+        SetFontID,
+        SetPaletteColorID,
+        RenderTextID,
+        RenderOpaqueTextID,
+        ClearScreenID,
+        ClearScreenIgnoreClippingID,
+        DrawPixelID,
+        DrawLineID,
+        DrawRectID,
+        DrawCircleID,
+        DrawTriangleID,
+        DrawTexturedTriangleID,
+        DrawMapID,
+        DrawSpriteID,
+        DrawBitmapID,
+        SetClippingID,
+        DisableClippingID,
+    };
+
     // API
     void SetFont(int width, int height, int offset);
-    void RenderOpaqueText(const char *text, int x, int y, int colorIndex, int16_t backgroundColorIndex);
+    void SetPaletteColor(int index, int r, int g, int b);
     void RenderText(const char *text, int x, int y, int colorIndex);
+    void RenderOpaqueText(const char *text, int x, int y, int colorIndex, int16_t backgroundColorIndex);
     void ClearScreen(uint8_t colorIndex = 0);
     void ClearScreenIgnoreClipping(uint8_t colorIndex = 0);
+    void DrawPixel(int x, int y, uint8_t colorIndex);
     void DrawLine(int x0, int y0, int x1, int y1, int color);
-    void DrawCircle(int x, int y, int radius, int color, bool filled);
     void DrawRect(int x, int y, int width, int height, int color, bool filled);
+    void DrawCircle(int x, int y, int radius, int color, bool filled);
     void DrawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, int color, bool filled);
     void DrawTexturedTriangle(int x0, int y0, int x1, int y1, int x2, int y2, int u0, int v0, int u1, int v1, int u2, int v2);
-    void DrawPixel(int x, int y, uint8_t colorIndex);
-    void SetClipping(int x0, int y0, int x1, int y1);
-    void DisableClipping();
     void DrawMap(int screenX, int screenY, int mapX, int mapY, int width, int height, int16_t transparentColorIndex);
     void DrawSprite(int x, int y, int spritex, int spritey, int width, int height, int16_t transparentColorIndex);
     void DrawBitmap(int screenPosX, int screenPosY, int bitmapPosX, int bitmapPosY, int width, int height, int pitch, int16_t transparentColorIndex = -1);
-    void SetPaletteColor(int index, int r, int g, int b);
+    void SetClipping(int x0, int y0, int x1, int y1);
+    void DisableClipping();
 
     // Helper functions
     uint32_t GetPaletteColor(uint8_t colorIndex);
