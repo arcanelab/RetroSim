d68a781d5b90fcdb1c808993ed6c7e7a14e70801
Zoltan Majoros <zoltan@arcanelab.com>
2023-10-09 00:39:57 +0200

    Removed superfluous include statement

--- src/sdl/SDLApp.cpp
+++ src/sdl/SDLApp.cpp
@@ -1,7 +1,6 @@
 // RetroSim - Copyright 2011-2023 Zoltán Majoros. All rights reserved.
 // https://github.com/arcanelab
 
-#include "GravityScripting.h"
 #include "SDLApp.h"
 #include "CoreConfig.h"
 #include "Core.h"
