e6ee2c4295da3f6df376c07b4bc37c9ba0c97f4f
Zoltan Majoros <zoltan@arcanelab.com>
2023-10-19 03:18:17 +0200

    Renamed 'network'

--- xmake.lua
+++ xmake.lua
@@ -42,9 +42,9 @@ local _target = Target.sdlgpu
 function AddTelnetDependencies() 
     add_defines("TELNET_ENABLED")
     add_includedirs("src/extern/libtelnet")
-    add_files("src/networking/*.cpp")
+    add_files("src/network/*.cpp")
     add_files("src/extern/libtelnet/*.c")
-    add_includedirs("src/networking")
+    add_includedirs("src/network")
 end
 
 function AddCommon()
