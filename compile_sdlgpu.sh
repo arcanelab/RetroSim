860b33806509b2e9b89da14dd79645fc99b5fdc6
Zoltan Majoros <zoltan@arcanelab.com>
2023-10-22 18:20:18 +0200

    Automatized building of SDL_GPU, cleaned up build script

---
+++ compile_sdlgpu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if build/SDL-gpu/lib/libSDL2_gpu.a exists
+if [ -f src/extern/sdl-gpu/build/SDL_gpu/lib/libSDL2_gpu.a ]; then
+    echo "SDL_gpu already compiled."
+    exit 0
+fi
+
+echo "Compiling SDL_gpu..."
+ls -l
+
+# Navigate to the specified directory
+mkdir -p ./src/extern/sdl-gpu/build
+cd ./src/extern/sdl-gpu/build
+
+# Check if cd command was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Couldn't change to the desired directory."
+    exit 1
+fi
+
+# Run cmake
+cmake .. -G "Unix Makefiles" -DBUILD_FRAMEWORK=OFF
+make
+
+cd ../../../../
+
+# Check if cmake command was successful
+if [ $? -ne 0 ]; then
+    echo "Error: cmake failed."
+    exit 1
+fi
+
+echo "cmake ran successfully!"
--- xmake.lua
+++ xmake.lua
@@ -5,16 +5,24 @@ add_rules("mode.debug", "mode.release")
 add_requires("libsdl 2.x")
 set_warnings("all")
 
+-- libraries built from submodules
+includes("src/extern/gravity")
+includes("src/cpu/A65000/AsmA65k")
+includes("src/extern/csid-mod")
+
+-- make sure to include debug symbols when appropriate
 if is_mode("debug") then
     add_defines("DEBUG")
     set_symbols("debug")
     set_optimize("none")
 else 
     set_symbols("hidden")
+    -- optionally we can include debug symbols in release builds, useful for profiling
     -- set_symbols("debug")
     set_optimize("fastest")
 end
 
+-- set platform-specific compiler settings
 if is_plat("windows") then
     set_warnings("all")
     add_defines("WIN32")
@@ -25,18 +33,6 @@ elseif is_plat("linux") or is_plat("macosx") or is_plat("mingw") then
     add_defines("SDL_GPU_DISABLE_GLES")
     -- add_cxflags("-Wall")
 end
-
-includes("src/extern/gravity")
-includes("src/cpu/A65000/AsmA65k")
-includes("src/extern/csid-mod")
-
-Target =
-{
-    sdl = 1,
-    sdlgpu = 2,
-}
-
-local _sdltarget = Target.sdl
 
 function AddTelnetDependencies() 
     add_defines("TELNET_ENABLED")
@@ -67,16 +63,8 @@ function AddCommon()
     set_targetdir("bin")
 end
 
--- How to build and include SDL_gpu on macOS:
--- * Place sdl-gpu in src/extern/sdl-gpu
--- * Build SDL_gpu with cmake:
---   - Modify CMakeLists.txt to build static library (option(BUILD_FRAMEWORK "Build SDL_gpu as Apple framework" ON)), then
---   cmake -G "Unix Makefiles"
---   make
--- * Library will be placed in src/extern/sdl-gpu/SDL_gpu/lib
--- * Include files will be placed in src/extern/sdl-gpu/SDL_gpu/include
--- For Windows: use cmake-gui
 function AddSDL_GPU()
+    add_deps("BuildSDL_gpu")    
     if is_plat("windows") then
         if is_mode("debug") then
             add_linkdirs("src/extern/sdl-gpu/build/SDL_gpu-VS-0.11.0/lib/Debug")
@@ -87,7 +75,7 @@ function AddSDL_GPU()
         add_links("sdl2_gpu_s")
         -- add_links("GL")
     elseif is_plat("macosx") then
-        add_linkdirs("src/extern/sdl-gpu/SDL_gpu/lib/")
+        add_linkdirs("src/extern/sdl-gpu/build/SDL_gpu/lib/")
         add_frameworks("OpenGL")
         add_links("sdl2_gpu")
     end
@@ -96,7 +84,17 @@ function AddSDL_GPU()
     add_includedirs("src/extern/sdl-gpu/include")
 end
 
+-- We need to know which SDL target we're building for.
+Target =
+{
+    sdl = 1,
+    sdlgpu = 2,
+}
+
+local _sdltarget = Target.sdlgpu
+
 target("RetroSim")
+    set_default(true)
     AddCommon()
     set_kind("binary")
     add_packages("libsdl")
@@ -122,75 +120,18 @@ target("RetroSimCore")
     else 
         set_targetdir("bin")
     end
--- end
 
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro definition
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
+target("BuildSDL_gpu")
+    set_default(false) -- This ensures it's not built with the default `xmake` command.
+    if not is_plat("windows") then
+        on_build(function()
+            if not os.exists("src/extern/sdl-gpu/SDL_gpu/lib/libSDL_gpu.a") then
+                print("Invoking cmake for src/extern/SDL-gpu")
+                os.exec("sh compile_sdlgpu.sh")
+            else
+                print("SDL_gpu already compiled.")
+            end
+        end)
+    else
+        -- Implement cmake build for Windows
+    end
