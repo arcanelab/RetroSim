bf400c2d316f0832a8e6d429a6b5e11df24d53e8
arcanelab <zoltan@arcanelab.com>
2024-06-30 14:03:05 +0200

    Fixed DPI scaling detection for Windows

--- src/system/raylib/DesktopDPI.cpp
+++ src/system/raylib/DesktopDPI.cpp
@@ -2,7 +2,8 @@
 // https://github.com/arcanelab
 
 #if defined(_WIN32)
-#include <windows.h>
+#include "windows.h"
+#include <cmath>
 #endif
 
 #if defined(__APPLE__)
@@ -15,9 +16,11 @@ namespace RetroSim
     float GetDesktopScalingFactor()
     {
 #if defined(_WIN32)
-        HWND hwnd = GetConsoleWindow(); // Or get your actual game window handle if not using the console
+        HWND hwnd = GetForegroundWindow();
         int dpi = GetDpiForWindow(hwnd);
-        return (float)dpi / 96.0f;
+        float scalingFactor = static_cast<float>(dpi) / 96.0f;
+        // if it's 0, return 1.
+        return std::fpclassify(scalingFactor) == FP_ZERO ? 1.0f : scalingFactor;
 #else
         // On Mac it's always 1.0f.
         // TODO: implement Linux, etc.
